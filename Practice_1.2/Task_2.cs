3d605e0feace7c77933d24a95d9a43d972b9e879
sssenya <senia7@bk.ru>
2024-01-02 14:34:42 +0300

    1.4 Практическая работа

---
+++ Practice_1.2/Task_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_1_2
+{
+    internal class Task_2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
--- Practice_1/Task_1.cs
+++ Practice_1/Task_1.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Practice_1
+namespace Practice_1_1
 {
-    internal class Program
+    internal class Task_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
         }
     }
 }
