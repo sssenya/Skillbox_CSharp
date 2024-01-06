8db6c286d5fa79645d69cd05ab2fca94ec59cbb6
sssenya <senia7@bk.ru>
2024-01-06 13:39:06 +0300

    3.10 Практическая работа - 1

---
+++ Practice_3_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            string userInput = Console.ReadLine();
+            int userInputParsed = int.Parse(userInput);
+
+            if (userInputParsed % 2 == 0)
+            {
+                Console.WriteLine("Введено четное число");
+            }
+            else
+            {
+                Console.WriteLine("Введено нечетное число");
+            }
+            Console.ReadKey();
+        }
+    }
+}
