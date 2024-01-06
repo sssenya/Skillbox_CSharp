fc1b1cacd36495b04ce6167185d96d6046eafd78
sssenya <senia7@bk.ru>
2024-01-06 14:11:02 +0300

    3.10 Практическая работа - 2

---
+++ Practice_3_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Введите количество у вас на руках:");
+            string amountUserInput = Console.ReadLine();
+            int cardsAmount = int.Parse(amountUserInput);
+
+            int sum = 0;
+
+            for (int i = 0; i < cardsAmount; i++)
+            {
+                Console.WriteLine("Введите номинал карты:");
+                string cardUserInput = Console.ReadLine();
+
+                switch (cardUserInput)
+                {
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                    case "10":
+                        sum += int.Parse(cardUserInput);
+                        break;
+
+                    case "J":
+                    case "Q":
+                    case "K":
+                    case "T":
+                        sum += 10;
+                        break;
+                }
+            }
+            Console.WriteLine($"Сумма ваших карт {sum}");
+            Console.ReadKey();
+
+        }
+    }
+}
