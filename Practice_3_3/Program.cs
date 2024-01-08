ff739bdf63ac4aea570a1a1c37da035d12797dc2
sssenya <senia7@bk.ru>
2024-01-08 10:59:15 +0300

    3.10 Практическая работа - 3

---
+++ Practice_3_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Введите число:");
+            string userInput = Console.ReadLine();
+            int userNumber = int.Parse(userInput);
+
+            bool isPrimeNumber = true;
+            int i = 2;
+
+            while (i < userNumber - 1)
+            {
+                if(userNumber % i == 0)
+                {
+                    isPrimeNumber = false;
+                    break;
+                }
+                i++;
+            }
+
+            if(isPrimeNumber)
+            {
+                Console.WriteLine("Введено простое число");
+            }
+            else
+            {
+                Console.WriteLine("Введено не простое число");
+            }
+            Console.ReadKey();
+        }
+    }
+}
