2d7856ea8ccdb2e3f24e1ad0d833ca3e02c1ddf0
sssenya <senia7@bk.ru>
2024-01-09 18:11:42 +0300

    3.10 Практическая работа - 4

---
+++ Practice_3_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Введите длину последовательности:");
+            string userLengthInput = Console.ReadLine();
+            int lengthOfSequence = int.Parse(userLengthInput);
+
+            int minValue = int.MaxValue;
+
+            for (int i = 0; i < lengthOfSequence; i++)
+            {
+                Console.WriteLine($"Введите {i+1}-й элемент последовательности:");
+                string userInput = Console.ReadLine();
+                int userNumber = int.Parse(userInput);
+
+                if (userNumber < minValue)
+                {
+                    minValue = userNumber;
+                }
+            }
+
+            Console.WriteLine($"Наименьший элемент в вашей последовательности: {minValue}");
+            Console.ReadKey();
+        }
+    }
+}
