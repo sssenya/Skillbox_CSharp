45f07892a2838716eb4e9c0bcebad52bb507e893
sssenya <senia7@bk.ru>
2024-01-10 23:25:31 +0300

    4.8 Практическая работа - 1

---
+++ Practice_4_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Введите количество строк в матрице:");
+            string userLinesAmount = Console.ReadLine();
+            int linesAmount = int.Parse(userLinesAmount);
+
+            Console.WriteLine("Здравствуйте! Введите количество столбцов в матрице:");
+            string userColumnsAmount = Console.ReadLine();
+            int columnsAmount = int.Parse(userColumnsAmount);
+
+            int[,] matrix = new int[linesAmount, columnsAmount];
+            Random random = new Random();
+            int sum = 0;
+
+            for (int i = 0; i < linesAmount; i++)
+            {
+                for (int j = 0; j < columnsAmount; j++)
+                {
+                    matrix[i, j] = random.Next(1000);
+                    sum += matrix[i, j];
+                    
+                    Console.Write($"{matrix[i, j], 6}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Сумма элементов матрицы равна {sum}");
+            Console.ReadKey();
+        }
+    }
+}
