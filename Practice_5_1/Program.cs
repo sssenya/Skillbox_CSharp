1d9c33982dab8ad2f4f45b8c408dcf08f7ecba50
sssenya <senia7@bk.ru>
2024-01-14 23:54:15 +0300

    5.5 Практическая работа - 1

---
+++ Practice_5_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Введите текст:");
+            string userText = Console.ReadLine();
+
+            string[] wordsFromText = SplitText(userText);
+            PrintWords(wordsFromText);
+
+            Console.ReadKey();
+        }
+
+        static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+
+        static void PrintWords(string[] text)
+        {
+            foreach (string word in text)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
