c2ffc69ff23d7397366c84accfc11ee9cdcca65a
sssenya <senia7@bk.ru>
2024-02-02 00:05:00 +0300

    8.6 Практическая работа - 3

---
+++ Practice_8_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_8_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+            string userInput;
+
+            do
+            {
+                Console.WriteLine("Введите целое число:");
+                userInput = Console.ReadLine();
+                int.TryParse(userInput, out var userInt);
+
+                if (set.Add(userInt))
+                {
+                    Console.WriteLine("Число добавлено!");
+                }
+                else
+                {
+                    Console.WriteLine("Число уже присутствует в коллекции!");
+                }
+            }
+            while (!string.IsNullOrEmpty(userInput));            
+        }
+    }
+}
