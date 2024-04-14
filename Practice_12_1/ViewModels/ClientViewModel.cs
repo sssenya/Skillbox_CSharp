770ab5008a6e633def199933cdf2fd850d2661aa
sssenya <senia7@bk.ru>
2024-04-14 22:05:30 +0300

    12.5 Практическая работа - добавлены команды для работы со счетами

--- Practice_12_1/ViewModels/ClientViewModel.cs
+++ Practice_12_1/ViewModels/ClientViewModel.cs
@@ -1,27 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 using Practice_12_1.Models;
+using Practice_12_1.Models.Interfaces;
+using Practice_12_1.Commands;
+using System.Windows.Documents;
+using System.Collections.Generic;
+using Practice_12_1.Models.Accounts;
 
 namespace Practice_12_1.ViewModels
 {
-    internal class ClientViewModel
+    internal class ClientViewModel : BaseViewModel
     {
         private readonly Client _client;
-        
+        private List<BankAccount> _accounts;
+
+        private IBankAccount _selectedAddMoneyAccount;
+
+
         public ClientViewModel(Client client)
         {
             _client = client;
+            UpdateAccountsList();
+
+            OpenDepAccCommand = new RelayCommand(obj => OpenNewDepositAccount(), obj => CanOpenDepositAcc());
+            OpenNonDepAccCommand = new RelayCommand(obj => OpenNewNonDepositAccount(), obj => CanOpenNonDepositAcc());
+            AddMoneyCommand = new RelayCommand(obj => AddMoneyToAccount());
         }
 
-        public ICommand OpenAccount { get; set; }
+        public ICommand OpenDepAccCommand{ get; set; }
+        public ICommand OpenNonDepAccCommand{ get; set; }
         public ICommand CloseAccount { get; set; }
         public ICommand MoveBetweenAccounts { get; set; }
-        public ICommand PutMoney { get; set; }
+        public ICommand AddMoneyCommand { get; set; }
         public ICommand MoveToClient { get; set; }
 
         public string FirstName => _client.FirstName;
@@ -37,5 +47,69 @@ namespace Practice_12_1.ViewModels
         public double NonDepAccountSum => _client.NonDepositAccount == null ? 0 : _client.NonDepositAccount.Balance;
         public string NonDepAccountDate => _client.NonDepositAccount == null ? "" : _client.NonDepositAccount.OpeningDate.ToShortDateString();
         public double NonDepAccountPercent => _client.NonDepositAccount == null ? 0 : _client.NonDepositAccount.Percent;
+
+        public List<BankAccount> Accounts
+        {
+            get => _accounts;
+            set => RaiseAndSetIfChanged(ref _accounts, value);
+        }
+
+        public IBankAccount SelectedAddMoneyAccount
+        {
+            get => _selectedAddMoneyAccount;
+            set => RaiseAndSetIfChanged(ref _selectedAddMoneyAccount, value);
+        }
+
+        public void OpenNewDepositAccount()
+        {
+            _client.DepositAccount = new DepositAccount();
+            UpdateAccountsList();
+            OnPropertyChanged(nameof(Accounts));
+        }
+
+        public bool CanOpenDepositAcc()
+        {
+            if(_client.DepositAccount == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void OpenNewNonDepositAccount()
+        {
+            _client.NonDepositAccount = new NonDepositAccount();
+            UpdateAccountsList();
+            OnPropertyChanged(nameof(Accounts));
+        }
+
+        public bool CanOpenNonDepositAcc()
+        {
+            if (_client.NonDepositAccount == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void AddMoneyToAccount()
+        {
+            AccountCalculator<IBankAccount>.AddMoney(SelectedAddMoneyAccount, 100);
+            OnPropertyChanged(nameof(DepAccountSum));
+            OnPropertyChanged(nameof(NonDepAccountSum));
+        }
+
+        private void UpdateAccountsList()
+        {
+            _accounts = new List<BankAccount>();
+            if (_client.DepositAccount != null)
+            {
+                _accounts.Add(_client.DepositAccount);
+            }
+            if (_client.NonDepositAccount != null)
+            {
+                _accounts.Add(_client.NonDepositAccount);
+            }
+        }
     }
 }
