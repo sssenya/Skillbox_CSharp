47f17cf1829f662665fd484c972b7e155876f2df
sssenya <senia7@bk.ru>
2024-04-17 17:30:20 +0300

    12.5 Практическая работа - добавлен класс и интерфейс для перевода между счетами

---
+++ Practice_12_1/Models/Accounts/IMoneyMover.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_12_1.Models
+{
+    internal interface IMoneyMover<in T>
+        where T : BankAccount
+    {
+        void SetAccounts(T accountFrom, T accountTo);
+
+        void MoveMoney(double sum);
+    }
+}
---
+++ Practice_12_1/Models/Accounts/MoneyMover.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Practice_12_1.Models
+{
+    internal class MoneyMover<T> : IMoneyMover<T> 
+        where T : BankAccount
+    {
+        private T _accountFrom;
+        private T _accountTo;
+
+        public void SetAccounts(T accountFrom, T accountTo)
+        {
+            _accountFrom = accountFrom;
+            _accountTo = accountTo;
+        }
+
+        public void MoveMoney(double sum)
+        {
+            bool canRemove = _accountFrom.RemoveMoney(sum);
+            if (canRemove)
+            {
+                _accountTo.AddMoney(sum);
+                MessageBox.Show("Перевод выполнен");
+            }
+            else
+            {
+                MessageBox.Show("Недостаточно средств для перевода");
+            }
+        }
+    }
+}
