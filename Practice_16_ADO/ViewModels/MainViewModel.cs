cdd3824dec44cfb3983f11a57888a86ee27963c3
sssenya <senia7@bk.ru>
2024-05-19 12:18:49 +0300

    16.7 Практическая работа - добавлено подключение к БД

---
+++ Practice_16_ADO/ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_16_ADO.ViewModels
+{
+    internal class MainViewModel
+    {
+        
+        
+        public MainViewModel()
+        {
+            SetConnection();
+        }
+
+        public void SetConnection()
+        {
+            SqlConnectionStringBuilder connectionStr = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(localdb)\MSSQLLocalDB",
+                InitialCatalog = "MSSQLTest",
+                IntegratedSecurity = true
+            };
+
+            SqlConnection connection = new SqlConnection()
+            {
+                ConnectionString = connectionStr.ConnectionString
+            };
+
+            connection.StateChange +=
+                (s, e) => { ConnectionStateMSSQL = (s as SqlConnection).State.ToString(); };
+
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public string ConnectionStateMSSQL { get; set; }
+    }
+}
