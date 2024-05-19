69235800df68a8941da4b4acc09fe3e3c9d522f2
sssenya <senia7@bk.ru>
2024-05-19 12:18:20 +0300

    16.7 Практическая работа - создан GUI

---
+++ Practice_16_ADO/Views/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Practice_16_ADO.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Practice_16_ADO
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
