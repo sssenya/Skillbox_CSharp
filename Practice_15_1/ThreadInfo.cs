59d8d9fd7b3c808f252c786dfab245147d9a95f1
sssenya <senia7@bk.ru>
2024-05-28 16:21:34 +0300

    15.11 Практическая работа - создан класс для вывода текста и информации о потоке

---
+++ Practice_15_1/ThreadInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Practice_15_1 {
+    internal class ThreadInfo {
+        public static void PrintThreadInfo(string threadName, 
+                                           string threadMessage,
+                                           int sleepTime) {
+            Thread currentThread = Thread.CurrentThread;
+            currentThread.Name = threadName;
+
+            for(int i = 0; i < 10; i++) {
+
+                Console.WriteLine($"MethodMessage: {threadMessage} /// ThreadName: {currentThread.Name}");
+                Thread.Sleep(sleepTime);
+            }
+
+            Console.WriteLine($"__________Thread \"{currentThread.Name}\" закончился__________");
+        }
+    }
+}
