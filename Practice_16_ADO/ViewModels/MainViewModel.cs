af1ff0bff4d115fb2a147d57b62a74ca54b79cb1
sssenya <senia7@bk.ru>
2024-05-31 12:22:30 +0300

    16.7 Практическая работа - добавлено подключение OleDbConnection

--- Practice_16_ADO/ViewModels/MainViewModel.cs
+++ Practice_16_ADO/ViewModels/MainViewModel.cs
@@ -1,51 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Practice_16_ADO.ViewModels
 {
     internal class MainViewModel
     {
-        
-        
         public MainViewModel()
         {
-            SetConnection();
+            SetMSSQLConnection();
+            SetAccessConnection();
         }
 
-        public void SetConnection()
+        public void SetMSSQLConnection()
         {
-            SqlConnectionStringBuilder connectionStr = new SqlConnectionStringBuilder()
-            {
+            SqlConnectionStringBuilder connectionStr = new SqlConnectionStringBuilder() {
                 DataSource = @"(localdb)\MSSQLLocalDB",
                 InitialCatalog = "MSSQLTest",
                 IntegratedSecurity = true
             };
 
-            SqlConnection connection = new SqlConnection()
-            {
-                ConnectionString = connectionStr.ConnectionString
+            SqlConnection connection = new SqlConnection() {
+                ConnectionString = connectionStr.ConnectionString,
             };
 
             connection.StateChange +=
                 (s, e) => { ConnectionStateMSSQL = (s as SqlConnection).State.ToString(); };
 
-            try
-            {
+            try {
                 connection.Open();
             }
-            catch (Exception)
-            {
+            catch (Exception e) {
+                ConnectionStateMSSQL = e.Message;
             }
-            finally
-            {
+            finally {
+                connection.Close();
+            }
+        }
+
+        public void SetAccessConnection() {
+            OleDbConnection connection = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=C:\AccessLocalDB\AccessLocalDB.mdb"); 
+
+            connection.StateChange +=
+                (s, e) => { ConnectionStateMSAccess = (s as OleDbConnection).State.ToString(); };
+
+            try {
+                connection.Open();
+            }
+            catch(Exception e) {
+                ConnectionStateMSAccess = e.Message;
+            }
+            finally {
                 connection.Close();
             }
         }
 
         public string ConnectionStateMSSQL { get; set; }
+        public string ConnectionStateMSAccess { get; set; }
     }
 }
