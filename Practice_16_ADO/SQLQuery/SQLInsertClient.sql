e01ab9283a08b8cbe75c3c4300bca52c38045854
sssenya <senia7@bk.ru>
2024-06-03 16:57:20 +0300

    16.7 Практическая работа - добавлен SQL запрос

---
+++ Practice_16_ADO/SQLQuery/SQLInsertClient.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO 
+[dbo].[Clients] ([Id], [SecondName], [FirstName], [MiddleName], [PhoneNumber], [Email]) 
+VALUES 
+(8, N'Иванов', N'Петр', N'Петрович', 8903366, N'ivanovpetr@mail.ru')
