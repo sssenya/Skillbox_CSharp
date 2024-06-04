a6326d442dc3f2d901bbff9d4948b1da479256f2
sssenya <senia7@bk.ru>
2024-06-04 18:21:54 +0300

    16.7 Практическая работа - добавлена команда для удаления клиентов из БД

--- Practice_16_ADO/ViewModels/MainViewModel.cs
+++ Practice_16_ADO/ViewModels/MainViewModel.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Controls;
 
 using Practice_10_1.ViewModels;
 using Practice_10_1.Commands;
@@ -23,6 +24,12 @@ namespace Practice_16_ADO.ViewModels
         private string _connectionStringMSSQL;
         private string _connectionStringMSAccess;
 
+        private DataRowView _selectedClient;
+
+        private SqlConnection _connection;
+        private SqlDataAdapter _sqlDataAdapter;
+        private DataTable _dataTable;
+
         public MainViewModel()
         {
             string workingDirectory = Environment.CurrentDirectory;
@@ -33,9 +40,11 @@ namespace Practice_16_ADO.ViewModels
             SetAccessConnection();
 
             OpenInfoWindow = new RelayCommand(obj => OpenConnectionInfoWindow());
+            DeleteClient = new RelayCommand(obj => DeleteSelectedClient());
         }
 
         public ICommand OpenInfoWindow { get; set; }
+        public ICommand DeleteClient { get; set; }
 
         public void SetMSSQLConnection()
         {
@@ -49,15 +58,34 @@ namespace Practice_16_ADO.ViewModels
 
             SqlConnection connection = new SqlConnection(_connectionStringMSSQL);
 
-            DataTable dataTable = new DataTable();
-            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            _dataTable = new DataTable();
+            _sqlDataAdapter = new SqlDataAdapter();
 
-            string sql = @"SELECT * FROM Clients Order By Clients.Id";
-            dataAdapter.SelectCommand = new SqlCommand(sql, connection);
+            string sqlSelect = @"SELECT * FROM Clients Order By Clients.Id";
+            _sqlDataAdapter.SelectCommand = new SqlCommand(sqlSelect, connection);
 
-            dataAdapter.Fill(dataTable);
+            string sqlUpdate = @"UPDATE Clients SET 
+                           SecondName = @SecondName,
+                           FirstName = @FirstName, 
+                           MiddleName = @MiddleName, 
+                           PhoneNumber = @PhoneNumber 
+                           Email = @Email 
+                    WHERE Id = @Id";
+            _sqlDataAdapter.UpdateCommand = new SqlCommand(sqlUpdate, connection);
+            _sqlDataAdapter.UpdateCommand.Parameters.Add("@Id", SqlDbType.Int, 4, "Id");
+            _sqlDataAdapter.UpdateCommand.Parameters.Add("@SecondName", SqlDbType.NVarChar, 20, "SecondName");
+            _sqlDataAdapter.UpdateCommand.Parameters.Add("@FirstName", SqlDbType.NVarChar, 20, "FirstName");
+            _sqlDataAdapter.UpdateCommand.Parameters.Add("@MiddleName", SqlDbType.NVarChar, 20, "MiddleName");
+            _sqlDataAdapter.UpdateCommand.Parameters.Add("@PhoneNumber", SqlDbType.Int, 4, "PhoneNumber");
+            _sqlDataAdapter.UpdateCommand.Parameters.Add("@Email", SqlDbType.NVarChar, 20, "Email");
 
-            ClientsDataTable = dataTable.DefaultView;
+            string sqlDelete = "DELETE FROM Clients WHERE Id = @Id";
+            _sqlDataAdapter.DeleteCommand = new SqlCommand(sqlDelete, connection);
+            _sqlDataAdapter.DeleteCommand.Parameters.Add("@Id", SqlDbType.Int, 4, "Id");
+
+
+            _sqlDataAdapter.Fill(_dataTable);
+            ClientsDataTable = _dataTable.DefaultView;
 
             connection.StateChange +=
                 (s, e) => { ConnectionStateMSSQL = (s as SqlConnection).State.ToString(); };
@@ -95,12 +123,22 @@ namespace Practice_16_ADO.ViewModels
         public string ConnectionStringMSSQL => _connectionStringMSSQL;
         public string ConnectionStringMSAccess => _connectionStringMSAccess;
 
+        public DataRowView SelectedClient {
+            get => _selectedClient;
+            set => RaiseAndSetIfChanged(ref _selectedClient, value);
+        }
+
         public DataView ClientsDataTable { get; set; }
 
         public void OpenConnectionInfoWindow() {
             ConnectionInfoViewModel connectionInfoVM = new ConnectionInfoViewModel(this);
             ConnectionInfoWindow window = new ConnectionInfoWindow(connectionInfoVM);
             window.ShowDialog();
+        }
+
+        public void DeleteSelectedClient() {
+            SelectedClient.Row.Delete();
+            _sqlDataAdapter.Update(_dataTable);
         }
     }
 }
