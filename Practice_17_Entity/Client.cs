943e1ef2240add76ef7a7ed47d410bbd91732a69
sssenya <senia7@bk.ru>
2024-06-13 19:47:13 +0300

    17.6 Практическая работа - созданы классы для работы с БД через Entity Framework

---
+++ Practice_17_Entity/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice_17_Entity;
+
+public partial class Client
+{
+    public int Id { get; set; }
+
+    public string SecondName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string MiddleName { get; set; } = null!;
+
+    public int? PhoneNumber { get; set; }
+
+    public string Email { get; set; } = null!;
+}
---
+++ Practice_17_Entity/MsqltestContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Practice_17_Entity;
+
+public partial class MsqltestContext : DbContext
+{
+    public MsqltestContext()
+    {
+    }
+
+    public MsqltestContext(DbContextOptions<MsqltestContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Initial Catalog=MSQLTest;Integrated Security=True;Pooling=False;Encrypt=True;Trust Server Certificate=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tmp_ms_x__3214EC07D69260BC");
+
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.MiddleName).HasMaxLength(50);
+            entity.Property(e => e.SecondName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
