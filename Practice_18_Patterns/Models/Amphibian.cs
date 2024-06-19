7c72317776a5e7bfc42fb9504623554d95e18943
sssenya <senia7@bk.ru>
2024-06-19 23:09:31 +0300

    18.5 Практическая работа - добавлены классы

---
+++ Practice_18_Patterns/Models/Amphibian.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_18_Patterns.Models
+{
+    class Amphibian : IAnimal {
+        public string Class { get; set; }
+        public string Order { get; set; }
+        public string Family { get; set; }
+        public string Species { get; set; }
+    }
+}
---
+++ Practice_18_Patterns/Models/Interfaces/IAnimal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_18_Patterns.Models
+{
+    interface IAnimal
+    {
+        string Class { get; set; }
+        string Order { get; set; }
+        string Family { get; set; }
+        string Species { get; set; }
+    }
+}
