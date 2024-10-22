3b25ae24fb697c7bbae2860ab84ac03acc01d765
sssenya <senia7@bk.ru>
2024-10-22 22:40:27 +0300

    19.8 Практическая работа - добавлен вывод списка контактов в таблицу

--- Practice_19_ASP/Controllers/StartViewController.cs
+++ Practice_19_ASP/Controllers/StartViewController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Practice_19_ASP.Models;
 
 namespace Practice_19_ASP.Controllers {
     public class StartViewController : Controller {
         public IActionResult Index() {
-            return View();
+            List<Contact> contacts = new List<Contact>() {
+                new Contact() { Id = 1000, Name = "hanme1ef", LastName = "sdfsdf" },
+                new Contact() { Id = 1001, Name = "gdfg", LastName = "sdfsf" },
+                new Contact() { Id = 1002, Name = "hanme1ef", LastName = "sfdf" },
+                new Contact() { Id = 1003, Name = "hanme1ef", LastName = "dsf" }
+            };
+            return View(contacts);
         }
     }
 }
