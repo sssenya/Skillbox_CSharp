ca7cbe26d3120bd05cba36253aa0204712284a62
sssenya <senia7@bk.ru>
2024-10-26 15:42:21 +0300

    19.8 Практическая работа - добавлена страница контакта

---
+++ Practice_19_ASP/Views/ContactInfo/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practice_19_ASP.Views.ContactInfo
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
