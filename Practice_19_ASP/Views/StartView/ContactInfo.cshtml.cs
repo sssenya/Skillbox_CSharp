95a5b4ced19bc667ac8990eda680ead65782fe01
sssenya <senia7@bk.ru>
2024-10-27 23:17:34 +0300

    19.8 Практическая работа - обновлены Views

--- Practice_19_ASP/Views/StartView/ContactInfo.cshtml.cs
+++ Practice_19_ASP/Views/StartView/ContactInfo.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Practice_19_ASP.Views.ContactInfo
+namespace Practice_19_ASP.Views.StartView
 {
-    public class IndexModel : PageModel
+    public class ContactInfoModel : PageModel
     {
         public void OnGet()
         {
