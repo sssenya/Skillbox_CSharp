84d3d04a5394109ea4442621ada35c74000ada0d
sssenya <senia7@bk.ru>
2024-11-07 21:12:20 +0300

    20.8 Практическая работа - добавлена страница создания нового контакта. Добавлен контроллер.

---
+++ Practice_19_ASP/Controllers/NewContactController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice_19_ASP.Models;
+
+namespace Practice_19_ASP.Controllers {
+    public class NewContactController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public Contact GetDataFromViewField(string name, 
+                                            string lastName, 
+                                            string middleName, 
+                                            string phone, 
+                                            string address,
+                                            string description) {
+            return new Contact() {
+                Id = 1000,
+                Name = name,
+                LastName = middleName,
+                MiddleName = middleName,
+                Phone = phone,
+                Address = address,
+                Description = description
+            };
+        }
+    }
+}
