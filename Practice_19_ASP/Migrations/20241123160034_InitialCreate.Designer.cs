6394a598649fc4c71d442f57596741d458f996b9
sssenya <senia7@bk.ru>
2024-11-23 19:51:31 +0300

    21.5 Практическая работа - обновлена миграция

--- Practice_19_ASP/Migrations/20241123160034_InitialCreate.Designer.cs
+++ Practice_19_ASP/Migrations/20241123160034_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Practice_19_ASP.Context;
 
@@ -11,9 +12,11 @@ using Practice_19_ASP.Context;
 namespace Practice_19_ASP.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241123160034_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
